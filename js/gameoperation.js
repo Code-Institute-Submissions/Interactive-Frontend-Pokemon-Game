f0fb07777eca14bd93b603ceae2b1a5cfb00dbde
OatsRx <otis.redman1992@gmail.com>
2020-06-16 09:57:33 +0000

    added main game operation function

---
+++ js/gameoperation.js
@@ -0,0 +1,14 @@
+//Game Operation will determine/manage layout of elements when clicked
+//setting up character selection and pre-battle stage.
+const GameOperation = {
+
+    //This is the main method for gamestate setup
+     setUpGame: function() {
+
+    //When clicked will call upon these methods to select character + set battle.
+       this.pokemonSelect();
+       this.setUpBattle();
+
+     }
+     
+    }
