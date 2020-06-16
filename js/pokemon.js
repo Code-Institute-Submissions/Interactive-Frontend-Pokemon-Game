557191ba426ec8ed7e377baf751febb600d8b7b8
OatsRx <otis.redman1992@gmail.com>
2020-06-16 10:29:21 +0000

    added attack methods for both pokemon and rocketPokmemon displaying health in real time

--- js/pokemon.js
+++ js/pokemon.js
@@ -72,3 +72,80 @@ let PokemonAbilities = {
       return attackValues;
 
     }
+
+      //The hitpoints will display current health of either pokemon during the battle.
+    let pokemonHp = document.querySelector(".pokemon-hitpoints");
+    let rocketPokemonHp = document.querySelector(".rocketPokemon-hitpoints");
+
+    if (pokemonSpeed >= rocketSpeed) {
+
+      // PokemonAttack will be the total of outputDamage + timesHit.
+      const pokemonAttackValues = pokemonAttack();
+      const dmgTotal = pokemonAttackValues[0] * pokemonAttackValues[1];
+      //console.log
+      //The hitpoints will display current health of either pokemon during the battle.
+      rocketPokemon.hitPoints = rocketPokemon.hitPoints - dmgTotal;
+
+      //I chose alerts to display battle data
+      alert("Your Pokemon hit " + pokemonAttackValues[0] + " damage " + pokemonAttackValues[1] + " times!");
+
+      if (rocketPokemon.hitPoints <= 0) {
+        alert("You win! Refresh browser to play again.");
+        pokemonHp.innerHTML = 'HP: ' + pokemon.hitPoints;
+        rocketPokemonHp.innerHTML = 'HP: 0';
+      } else {
+        rocketPokemonHp.innerHTML = 'HP: ' + rocketPokemon.hitPoints;
+
+        // RocketAttack will be the total of outputDamage + timesHit.
+        const rocketAttackValues = rocketAttack();
+        const dmgTotal = rocketAttackValues[0] * rocketAttackValues[1];
+
+        pokemon.hitPoints = pokemon.hitPoints - dmgTotal;
+        alert("Rocket's Pokemon attacked with " + rocketAttackValues[0] + " damage " + rocketAttackValues[1] + " times!");
+
+        //This if, else, else if statement determines what to display if the player loses or wins the battle
+        if (pokemon.hitPoints <= 0) {
+          alert("You lost the battle :( Refresh the page to try again!");
+
+          // set HP to 0 to stop minus numbers incase of overkill turn
+          pokemonHp.innerHTML = 'HP: 0';
+          rocketPokemonHp.innerHTML = 'HP: ' + rocketPokemon.hitPoints;
+        } else {
+          pokemonHp.innerHTML = 'HP: 0' + pokemon.hitPoints;
+        }
+      }
+    } else if (rocketSpeed >= pokemonSpeed) {   //if the opponent attacks first.
+
+      const rocketAttackValues = rocketAttack();
+      const dmgTotal = rocketAttackValues[0] * rocketAttackValues[1];
+
+      pokemon.hitPoints = pokemon.hitPoints - dmgTotal;
+      alert("Rocket's Pokemon attacked with " + rocketAttackValues[0] + " damage " + rocketAttackValues[1] + " times!");
+
+      if (pokemon.hitPoints <= 0) {
+        alert("You lost the battle :( Refresh the page to try again!");
+
+        rocketPokemonHp.innerHTML = 'HP: ' + rocketPokemon.hitPoints;
+        pokemonHp.innerHTML = 'HP: 0';
+      } else {
+        pokemonHp.innerHTML = 'HP: ' + pokemon.hitPoints;
+                                                              // pokemon attacks
+        const pokemonAttackValues = pokemonAttack();
+        const dmgTotal = pokemonAttackValues[0] * pokemonAttackValues[1];
+
+        rocketPokemon.hitPoints = rocketPokemon.hitPoints - dmgTotal;
+
+        alert("You hit " + pokemonAttackValues[0] + " damage " + pokemonAttackValues[1] + " times!");
+
+        if (rocketPokemon.hitPoints <= 0) {
+          alert("You won the battle! To play again please refresh the page!");
+          rocketPokemonHp.innerHTML = 'HP: 0';
+          pokemonHp.innerHTML = 'HP: ' + pokemon.hitPoints;
+        } else {
+          rocketPokemonHp.innerHTML = 'HP: ' + rocketPokemon.hitPoints;
+        }
+      }
+    }
+  }
+
+}
