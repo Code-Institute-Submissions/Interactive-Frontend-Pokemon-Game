515326f7313a7fa8edacb0701b843a2c5a788ce1
OatsRx <otis.redman1992@gmail.com>
2020-06-16 10:22:48 +0000

    added attack calc method for pokemon player

--- js/pokemon.js
+++ js/pokemon.js
@@ -11,3 +11,42 @@ function Pokemon(pokemonName, hitPoints, attack, defense, special, speed) {
   this.speed = speed;
 
 }
+
+let PokemonAbilities = {
+
+  attackCalculation: function() { // we need to determine who will attack first
+
+    const pokemonSpeed = pokemon.speed;
+    const rocketSpeed = rocketPokemon.speed;
+
+    //Attack calculations for Pokemon based on Pokemons stats
+    //these statistics are base on real pokemon stats from the games
+    //and damage calculations based more on my methods rather than
+    //methods used in GameBoy games.
+    const pokemonAttack = function() {
+
+      let dmgCalc;
+
+      //Traditionally in Pokemon a character will attack if their speed is greater
+      //than their opponents, i wanted to change this slighty due to some speed being the
+      //same among my characters.
+      if (pokemon.special > 50) {
+
+        dmgCalc = pokemon.attack * pokemon.special / 1000 * 3;
+
+      } else {
+
+        dmgCalc = pokemon.attack * pokemon.speed / 1000 * 3;
+      }
+
+      const outputDamage = dmgCalc;
+
+     //So that we don't hit the same damage every turn the number of hits will vary.
+     //Random Number Generator (RNG) to select number of hits.
+     //the math.floor math.random equation for working out a random number was taken from w3Schools.
+      const timesHit = Math.floor(Math.random() * Math.floor(4)  + 1);
+      //console.log
+      const attackValues = [outputDamage, timesHit];
+
+      return attackValues;
+    }
